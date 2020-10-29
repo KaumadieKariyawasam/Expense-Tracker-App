7edcad13c7ebb7038a20f53c929b591163b8a6cf
KAW <kkawmadie@gmail.com>
2020-10-29 10:59:19 +0530

    make expense entity

---
+++ src/main/java/com/example/vizu/abc/model/Expense.java
@@ -0,0 +1,24 @@
+package com.example.vizu.abc.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "expense")
+public class Expense {
+    @Id
+    private Long id;
+    private Date expenseDate;
+    private String description;
+    
+}
