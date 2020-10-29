061f1b046f34882352e9040d3b9eee5132b520e2
KAW <kkawmadie@gmail.com>
2020-10-29 11:18:47 +0530

    make category repository

--- src/main/java/com/example/vizu/abc/repository/CategoryRepository.java
+++ src/main/java/com/example/vizu/abc/repository/CategoryRepository.java
@@ -1,5 +1,9 @@
 package com.example.vizu.abc.repository;
 
-public class CategoryRepository {
+import com.example.vizu.abc.model.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long>{
     
 }
