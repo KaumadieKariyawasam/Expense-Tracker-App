0e48bf0569ddf138e56229cff803ee7098a8c60f
KAW <kkawmadie@gmail.com>
2020-11-04 15:48:19 +0530

    implement post service

--- src/main/java/com/example/vizu/abc/controller/BlogController.java
+++ src/main/java/com/example/vizu/abc/controller/BlogController.java
@@ -1,10 +1,13 @@
 package com.example.vizu.abc.controller;
 
+import com.example.vizu.abc.service.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogController {
+
+    private PostService postService;
 
     @GetMapping(value = "/")
     public String getIndex(){
--- src/main/java/com/example/vizu/abc/service/PostService.java
+++ src/main/java/com/example/vizu/abc/service/PostService.java
@@ -1,4 +1,19 @@
 package com.example.vizu.abc.service;
 
+import com.example.vizu.abc.model.Post;
+import com.example.vizu.abc.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> getAllPost(){
+        return postRepository.findAll();
+    }
 }
